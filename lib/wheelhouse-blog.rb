a4f8e524b6f1330c6f975712da38ba38e9511457
Sam Pohlenz <sam@sampohlenz.com>
2011-11-28 20:22:34 +1030

    Add config option for blogs as sections

--- lib/wheelhouse-blog.rb
+++ lib/wheelhouse-blog.rb
@@ -2,6 +2,11 @@ require "wheelhouse"
 
 module Blog
   class Plugin < Wheelhouse::Plugin
+    config.wheelhouse.blog = ActiveSupport::OrderedOptions.new
+    
+    # Enable blog sections by default
+    config.wheelhouse.blog.sections = true
+    
     isolate_namespace Blog
     
     resources do
@@ -11,6 +16,12 @@ module Blog
     end
     
     resource { Resource(::Blog::Blog) }
+    
+    sections do
+      ::Blog::Blog.select(:id, :label).map do |blog|
+        Section(blog.label, blog)
+      end if ::Blog::Plugin.config.wheelhouse.blog.sections
+    end
     
     initializer "precompile assets" do |app|
       app.config.assets.precompile << "wheelhouse-blog/admin.*"
