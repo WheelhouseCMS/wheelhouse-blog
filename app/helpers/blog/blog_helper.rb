79067fffada001a213168dddac65fd994d681a56
Sam Pohlenz <sam@sampohlenz.com>
2012-08-02 15:32:15 +0930

    Add missing format_month helper

--- app/helpers/blog/blog_helper.rb
+++ app/helpers/blog/blog_helper.rb
@@ -2,4 +2,8 @@ module Blog::BlogHelper
   def blog_post_tag_links(post)
     post.tags.map { |tag| link_to(tag, post.blog.tag_path(tag), :title => "View all posts tagged #{tag}") }.join(', ').html_safe
   end
+
+  def format_month(year, month)
+    "#{Date::MONTHNAMES[month]} #{year}"
+  end
 end
