471aacc2cc6221a9b4fb31fe8be07d8d3fa4a1a9
Sam Pohlenz <sam@sampohlenz.com>
2012-09-14 15:00:53 +0930

    Add blog access helper

--- app/helpers/blog/blog_helper.rb
+++ app/helpers/blog/blog_helper.rb
@@ -1,4 +1,14 @@
 module Blog::BlogHelper
+  def blog(label=nil)
+    @_blogs ||= {}
+    
+    if label
+      @_blogs[label] ||= Blog::Blog.get(label)
+    else
+      @_blogs[:default] ||= Blog::Blog.first
+    end
+  end
+  
   def blog_post_tag_links(post)
     post.tags.map { |tag| link_to(tag, post.blog.tag_path(tag), :title => "View all posts tagged #{tag}") }.join(', ').html_safe
   end
