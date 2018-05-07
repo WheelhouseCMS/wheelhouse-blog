2a2c57879105a308d0b6e72a2d78d873d98f5307
Sam Pohlenz <sam@sampohlenz.com>
2018-05-07 13:00:53 +0930

    Pass controller parent model class name as string

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -1,5 +1,5 @@
 class Blog::PostsController < Wheelhouse::ResourceController
-  belongs_to :blog, :class_name => Blog::Blog
+  belongs_to :blog, :class_name => "Blog::Blog"
   defaults :resource_class => Blog::Post, :collection_name => :all_posts
 
   manage_site_breadcrumb
